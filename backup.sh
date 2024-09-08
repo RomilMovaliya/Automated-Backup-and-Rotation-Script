cfe32fa05247c7324a3b6829b3a98be14caef05b
Romil Movaliya <105592033+RomilMovaliya@users.noreply.github.com>
2024-09-08 11:27:34 +0530

    Update backup.sh
    updating backup file with curl status enable for testing.

--- backup.sh
+++ backup.sh
@@ -17,6 +17,9 @@ RETENTION_MONTHS=3 # To keeping the number of monthly backups.
 LOG_FILE="$DIRECTORY_OF_BACKUP/backup.log"
 WEBHOOK_URL="https://webhook.site/a5d27956-9dbc-4381-9af4-f844032beb07" # Just copy Webhook URL from Webhoook.site for notifications
 
+#It is curl status which is enabled by default.
+CURL_STATUS_ENABLED=${3:-true}
+
 # Now we Create a backup directory if it is does not exist
 mkdir -p "$DIRECTORY_OF_BACKUP"
 
@@ -26,28 +29,28 @@ git clone "$GITHUB_REPOSITORY" "$DIRECTORY_OF_PROJECT"
 # Create a backup
 zip -r "$DIRECTORY_OF_BACKUP/$NAME_OF_THE_BACKUP" "$DIRECTORY_OF_PROJECT"
 
-# Log the backup
+# when our backup file is created then it print the Log of backup and store it in logfile.
 echo "$(date): Created backup $NAME_OF_THE_BACKUP" >> "$LOG_FILE"
 
 # Here we are Integrating with Google Drive 
-GDRIVE_REMOTE="gdrive_backup" # Name of your rclone remote for Google Drive
-GDRIVE_FOLDER="BackupFolder"  # Google Drive folder name
+GDRIVE_REMOTE="gdrive_backup" # It is Name of rclone remote 
+GDRIVE_FOLDER="BackupFolder"  # It is Name of google drive folder
 
 # Push the backup to Google Drive
 rclone copy "$DIRECTORY_OF_BACKUP/$NAME_OF_THE_BACKUP" "$GDRIVE_REMOTE:$GDRIVE_FOLDER"
 
-# Log the upload
+# Here we are printing Log when our backup file is uploaded on google drive.
 echo "$(date): Uploaded backup $NAME_OF_THE_BACKUP to Google Drive" >> "$LOG_FILE"
 
 # Retention function
 rotate_backups() {
-    # Delete backups older than 'x' days
+    # It Delete backups older than 'x' days
     find "$DIRECTORY_OF_BACKUP" -type f -name "*.zip" -mtime +$RETENTION_DAYS -exec rm {} \;
     
-    # Keep last 4 weekly backups (e.g., last 4 Sundays)
+    # It Keeps last 4 weekly backups (e.g., last 4 Sundays)
     find "$DIRECTORY_OF_BACKUP" -type f -name "*.zip" -exec bash -c '(( $(date -d $(basename {} .zip | cut -d"_" -f2) +%u) == 7 ))' \;
 
-    # Keep last 3 monthly backups (e.g., first of each month)
+    # It Keeps last 3 monthly backups (e.g., first of each month)
     find "$DIRECTORY_OF_BACKUP" -type f -name "*.zip" -exec bash -c '(( $(date -d $(basename {} .zip | cut -d"_" -f2) +%d) == 1 ))' \;
 
     echo "$(date): Rotated backups" >> "$LOG_FILE"
@@ -55,7 +58,17 @@ rotate_backups() {
 
 rotate_backups
 
-# Notification sending using cURL
+
+
+curl_status_func(){
 curl -X POST -H "Content-Type: application/json" \
     -d '{"project": "'"ROMILBHAI'S PROJECT"'", "date": "'"$(date)"'", "test": "BackupSuccessful"}' \
     "$WEBHOOK_URL"
+}
+
+
+if [ "$CURL_STATUS_ENABLED" = "true" ]; then
+    curl_status_func
+else
+    echo "cURL notification is disabled, so no notification will be sent." >> "$LOG_FILE"
+fi
